fb20f3a1dae04f20bb8d7b0976a85761e01dc31a
Caike Araujo <caikecm.araujo@gmail.com>
2025-09-26 16:40:11 -0300

    feat: complement of header

--- fsw-barber/src/app/page.tsx
+++ fsw-barber/src/app/page.tsx
@@ -1,7 +1,11 @@
 import Header from "@/components/header"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
 import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+import { Badge } from "@/components/ui/badge"
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar"
 
 const Home = () => {
   return (
@@ -16,6 +20,40 @@ const Home = () => {
             <SearchIcon />
           </Button>
         </div>
+
+        <div className="relative mt-6 h-[150px] w-full rounded-xl">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="item-center flex gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage
+                    className="h-8 w-8 rounded-full"
+                    src="/avatar.png"
+                  />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
